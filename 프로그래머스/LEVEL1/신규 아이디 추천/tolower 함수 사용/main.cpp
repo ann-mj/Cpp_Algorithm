013df62ec263f40194a92b2e90c5283d75e42942
annmj <annmj.kr@gmail.com>
2022-03-15 19:38:06 +0900

    프로그래머스 신규 아이디 추천 누락 함수 추가

--- 프로그래머스/LEVEL1/신규 아이디 추천/tolower 함수 사용/main.cpp
+++ 프로그래머스/LEVEL1/신규 아이디 추천/tolower 함수 사용/main.cpp
@@ -3,64 +3,25 @@
 #include <vector>
 
 using namespace std;
+bool isok(char x) {
+    if (islower(x) || isdigit(x) || x == '-' || x == '_' || x == '.') return 1;
+    return 0;
+}
 
 string solution(string new_id) {
-    string answer = "";
-    
-    for(int i=0; i<new_id.length(); i++) {
-        // 대 -> 소
-        if(isupper(new_id[i])) {
-            new_id[i] = tolower(new_id[i]);
-            continue;
-        }
-        
-        // 소문자, 숫자, 빼기, 밑줄, 마침표 빼고 다 제거
-        if(new_id[i] >= 'a' && new_id[i] <= 'z') continue;
-        if(isdigit(new_id[i])) continue;
-        if(new_id[i] == '-' || new_id[i] == '_' || new_id[i] == '.') continue;
-        new_id.replace(i, 1, "");
-        i--;
-    }
-    
-    for(int i=0; i<new_id.length()-1; i++) {
-        if(new_id[i] == '.' && new_id[i+1] == '.') {
-            new_id.replace(i,2,".");
-            i--;
-        }
-    }
-    
-    if(new_id[0] == '.') {
-        new_id.replace(0,1,"");
-    }
-    
-    if(new_id[new_id.length()-1] == '.') {
-        new_id.replace(new_id.length()-1, 1,"");
-    }
-    
-    if(new_id.length() == 0) {
-        new_id.push_back('a');
-    }
-    
-    if(new_id.length() >= 16) {
-        new_id = new_id.substr(0, 15);
-    }
-    if(new_id[0] == '.') {
-        new_id.replace(0,1,"");
-    }
-    
-    if(new_id[new_id.length()-1] == '.') {
-        new_id.replace(new_id.length()-1, 1,"");
-    }
-    
-    if(new_id.length() <= 2) {
-        char last_char = new_id[new_id.length()-1];
-        while(new_id.length() != 3) {
-            new_id.push_back(last_char);
-        }
-    }
-    cout << new_id;
-    answer = new_id;
-    return answer;
+    string tmp ="";
+    for (char &c : new_id) if (isupper(c)) c = tolower(c);
+    for (char c : new_id) if (isok(c)) tmp += c;
+    new_id = "";
+    for (int i = 0; i < tmp.size(); i++) 
+        if (tmp[i] != '.' || (!new_id.empty() && new_id.back() != '.')) new_id += tmp[i];
+    if (new_id.back() == '.') new_id.pop_back();
+    if (new_id.front() == '.') new_id = new_id.substr(1);
+    if (new_id.empty()) new_id.push_back('a');
+    if (new_id.size() >= 16) new_id = new_id.substr(0, 15);
+    while (new_id.back() == '.') new_id.pop_back();
+    while (new_id.size() <= 2) new_id += new_id.back();
+    return new_id;
 }
 
 int main(void) {
