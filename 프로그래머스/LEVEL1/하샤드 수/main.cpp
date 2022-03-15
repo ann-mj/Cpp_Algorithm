20dba58779c416c9d16ffe1d5693546f53eaa690
annmj <annmj.kr@gmail.com>
2022-03-15 13:18:28 +0900

    프로그래머스 하샤드수 to_string() 버전

--- 프로그래머스/LEVEL1/하샤드 수/main.cpp
+++ 프로그래머스/LEVEL1/하샤드 수/main.cpp
@@ -23,3 +23,21 @@ int main(void) {
     cout << answer;
     return 0;
 }
+
+// to_string()
+bool solution2(int x) {
+    bool answer = true;
+    int s = 0;
+    string has = to_string(x);
+    for(int i=0; i<has.length(); i++){
+        string c;
+        c.push_back(has[i]);
+    s+=atoi(c.c_str());
+        c.clear();
+    }
+    if (x%s == 0)
+        answer=true;
+    else
+        answer=false;
+    return answer;
+}
