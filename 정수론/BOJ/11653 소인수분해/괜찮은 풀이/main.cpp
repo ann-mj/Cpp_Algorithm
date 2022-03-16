4d089d35cdff54d92567d0bf1d5b45182938f939
annmj <annmj.kr@gmail.com>
2022-03-16 14:19:48 +0900

    boj 11653 소인수분해 괜찮은 풀이 추가

---
+++ 정수론/BOJ/11653 소인수분해/괜찮은 풀이/main.cpp
@@ -0,0 +1,14 @@
+int main() {
+    int N;
+    cin >> N;
+ 
+    if (N == 1) return 0;
+ 
+    for (int i = 2; i <= N; i++) {
+        while (N % i == 0) {
+            cout << i << endl;
+            N /= i;
+        }
+    }
+ 
+}
